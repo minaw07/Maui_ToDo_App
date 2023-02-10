f1b875c8d18633bc51bca1dc2204ec633624cd8f
Clev_win <Clev_win@DESKTOP-JNB5KVU>
2023-02-10 22:41:19 +0100

    Add project files.

---
+++ ToDoMauiClient/DataServices/RestDataService.cs
@@ -0,0 +1,139 @@
+﻿using System.Text;
+using System.Text.Json;
+using ToDoMauiClient.Models;
+using Debug = System.Diagnostics.Debug;
+
+namespace ToDoMauiClient.DataServices
+{
+    public class RestDataService : IRestDataService
+    {
+        private readonly HttpClient _HttpClient;
+        private readonly string _BaseAddress;
+        private readonly string _Url;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public RestDataService()
+        {
+            _HttpClient = new HttpClient();
+            _BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5272" : "https://localhost:7077";
+            _Url = $"{_BaseAddress}/api";
+
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+        }
+        public async Task AddToDoAsync(ToDo todo)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("-----> No internet access");
+                return;
+            }
+            try
+            {
+                string jsonToDo = JsonSerializer.Serialize<ToDo>(todo,_jsonSerializerOptions);
+                StringContent content = new StringContent(jsonToDo, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _HttpClient.PostAsync($"{_Url}/todos", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("ToDo created successfully");
+                }
+                else
+                {
+                    Debug.WriteLine("-----> Non Http 2xx response");
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"Whoops exception :{ex.Message}");
+            }
+            return;
+        }
+
+        public async Task<List<ToDo>> GetAllToDosAsync()
+        {
+            List<ToDo> todos = new List<ToDo>();
+            if(Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("-----> No internet access");
+                return todos;
+            }
+            try
+            {
+                HttpResponseMessage response = await _HttpClient.GetAsync($"{_Url}/todos");
+                if(response.IsSuccessStatusCode)
+                {
+                    string context = await response.Content.ReadAsStringAsync();
+                    todos = JsonSerializer.Deserialize<List<ToDo>>(context, _jsonSerializerOptions);
+                }
+                else
+                {
+                    Debug.WriteLine("-----> Non Http 2xx response");
+                }
+            }catch(Exception ex) 
+            {
+                Debug.WriteLine($"Whoops exception :{ ex.Message}");
+            }
+            return todos;
+        }
+
+        public async Task RemoveToDoAsync(int id)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("-----> No internet access");
+                return;
+            }
+            try
+            {
+
+                HttpResponseMessage response = await _HttpClient.DeleteAsync($"{_Url}/todos/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("ToDo Deleted successfully");
+                }
+                else
+                {
+                    Debug.WriteLine("-----> ToDo is not deleted successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Whoops exception :{ex.Message}");
+            }
+            return;
+        }
+
+        public async Task UpdateToDoAsync(ToDo todo)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("-----> No internet access");
+                return;
+            }
+            try
+            {
+                string jsonToDo = JsonSerializer.Serialize<ToDo>(todo, _jsonSerializerOptions);
+                StringContent content = new StringContent(jsonToDo, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _HttpClient.PutAsync($"{_Url}/todos/{todo.Id}", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("ToDo Updated successfully");
+                }
+                else
+                {
+                    Debug.WriteLine("-----> Non Http 2xx response");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Whoops exception :{ex.Message}");
+            }
+            return;
+        }
+    }
+}
---
+++ ToDoMauiClient/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using ToDoMauiClient.DataServices;
+
+namespace ToDoMauiClient;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IRestDataService _dataservice;
+
+    public MainPage(IRestDataService dataservice)
+	{
+		InitializeComponent();
+		_dataservice = dataservice;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        collectionView.ItemsSource = await _dataservice.GetAllToDosAsync();  
+    }
+
+    async void OnAddToDoClicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("-----> Add button clicked");
+    }
+    async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine("-----> Item changed clicked");
+    }
+
+}
+
